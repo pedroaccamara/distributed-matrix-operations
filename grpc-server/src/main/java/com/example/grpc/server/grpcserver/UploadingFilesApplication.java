af6866117b3f043e256cea2bde0be4c759afdc47
pedroaccamara <pedroaccamara@gmail.com>
2022-04-16 19:56:45 +0100

    Repackaging errors

---
+++ grpc-server/src/main/java/com/example/grpc/server/grpcserver/UploadingFilesApplication.java
@@ -0,0 +1,27 @@
+package com.example.grpc.server.grpcserver;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.example.grpc.server.grpcserver.storage.StorageProperties;
+import com.example.grpc.server.grpcserver.storage.StorageService;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadingFilesApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+}
