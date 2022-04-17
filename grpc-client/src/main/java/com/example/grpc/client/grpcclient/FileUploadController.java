c9c72702bb69c2ed2f498dec1bcc174f7437fe74
pedroaccamara <pedroaccamara@gmail.com>
2022-04-17 19:20:16 +0100

    Matrix processing

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/FileUploadController.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/FileUploadController.java
@@ -2,6 +2,7 @@ package com.example.grpc.client.grpcclient;
 
 import java.io.IOException;
 import java.util.stream.Collectors;
+import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -56,15 +57,34 @@ public class FileUploadController {
 	public String handleFileUpload(@RequestParam("file") MultipartFile file,
 			RedirectAttributes redirectAttributes) {
 		if (file.isEmpty()) {
-			redirectAttributes.addFlashAttribute("message", "Please choose a file before uploading!");
-			return "redirect:/";
+			// redirectAttributes.addFlashAttribute("message", "Please choose a file before uploading!");
+			// return "redirect:/";
+			return message(redirectAttributes, "Please choose a file before uploading!");
+		}
+
+		try { // Deletables
+			String stringed = new String(file.getBytes());
+			String[] matrix = stringed.trim().split("\n");
+			String[] cols = matrix[0].trim().split(" ");
+			int[][] m = new int[matrix.length][cols.length];
+			if (!square(m)) return message(redirectAttributes, "Matrix is not squared");
+			if (!sidesSquared(m)) return message(redirectAttributes, "Matrix's sides aren't a perfect square");
+			System.out.println("Tried string way successfuly: ");
+			String conversion = toInt(m, matrix);
+			if (!conversion.equals("")) return message(redirectAttributes, conversion);
+			System.out.println("And converted to int successfuly: " + Arrays.deepToString(m));
+		}
+		catch (Exception e) {
+			System.out.println("FileUploadController@63\n" + e.toString());
+			return message(redirectAttributes, "Check error");
 		}
 
 		storageService.store(file);
-		redirectAttributes.addFlashAttribute("message",
-				"You successfully uploaded " + file.getOriginalFilename() + "!");
+		// redirectAttributes.addFlashAttribute("message",
+		// 		"You successfully uploaded " + file.getOriginalFilename() + "!");
 
-		return "redirect:/";
+		// return "redirect:/";
+		return message(redirectAttributes, "You successfully uploaded " + file.getOriginalFilename() + "!");
 	}
 
 	@ExceptionHandler(StorageFileNotFoundException.class)
@@ -72,4 +92,48 @@ public class FileUploadController {
 		return ResponseEntity.notFound().build();
 	}
 
+	private String message(RedirectAttributes redirectAttributes, String message) {
+		return message(redirectAttributes, message, "redirect:/");
+	}
+
+	private String message(RedirectAttributes redirectAttributes, String message, String redirect) {
+		redirectAttributes.addFlashAttribute("message", message);
+		return redirect;
+	}
+
+	private String toInt(int[][] m, String[] matrix) {
+		int noCols = matrix[0].trim().split(" ").length;
+		int r = 0;
+		int c = 0;
+		for (String row : matrix) {
+			for (String num : row.trim().split(" ")) {
+				m[r][c] = Integer.parseInt(num);
+				c += 1;
+				if (c == noCols) return "Columns don't all have the same size";
+			}
+			if (c != noCols-1) return "Columns don't all have the same size";
+			c = 0;
+			r += 1;
+		}
+		return "";
+	}
+
+	private boolean square(int[][] m) {
+		return m.length == m[0].length;
+	}
+
+	private boolean sidesSquared(int[][] m) {
+		int side = m.length;
+		if (side != 0) {
+			while (true) {
+				if (side % 2 == 0) {
+					side /= 2;
+					if (side == 1) return true;
+				}
+				else return false;
+			}
+		}
+
+		return false;
+	}
 }
