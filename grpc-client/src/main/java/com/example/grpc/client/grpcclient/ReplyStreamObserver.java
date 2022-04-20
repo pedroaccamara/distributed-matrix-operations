64cc12eb58f8c2844241e1fdd321e2966632cee2
pedroaccamara <pedroaccamara@gmail.com>
2022-04-20 03:27:37 +0100

    Overriding StreamObserver methods

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/ReplyStreamObserver.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/ReplyStreamObserver.java
@@ -2,13 +2,11 @@ package com.example.grpc.client.grpcclient;
 import io.grpc.stub.StreamObserver;
 import com.example.grpc.server.grpcserver.MatrixReply;
 
-public class ReplyStreamObserver implements StreamObserver<MatrixReply> {
+public abstract class ReplyStreamObserver implements StreamObserver<MatrixReply> {
 
 
     @Override
-    public void onNext(MatrixReply reply) {
-        System.out.println("Received from server: \n" + reply.getC00() + "\n" + reply.getC01() + "\n" + reply.getC10() + "\n" + reply.getC11() + "\n" );
-    }
+    public void onNext(MatrixReply reply) {}
 
     @Override
     public void onError(Throwable throwable) {}
