da60023d3a4b0f52b10d95b103bb95994c498fde
pedroaccamara <pedroaccamara@gmail.com>
2022-04-20 03:37:29 +0100

    Fixing ReplyStreamObserver

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/ReplyStreamObserver.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/ReplyStreamObserver.java
@@ -1,12 +1,14 @@
 package com.example.grpc.client.grpcclient;
 import io.grpc.stub.StreamObserver;
 import com.example.grpc.server.grpcserver.MatrixReply;
+// import com.example.grpc.client.grpcclient.MatrixReply;
 
-public abstract class ReplyStreamObserver implements StreamObserver<MatrixReply> {
-
+public class ReplyStreamObserver implements StreamObserver<MatrixReply> {
 
     @Override
-    public void onNext(MatrixReply reply) {}
+    public void onNext(MatrixReply reply) {
+        System.out.println("Received from server " + reply.getC00() + "\n" + reply.getC01() + "\n" + reply.getC10() + "\n" + reply.getC11() + "\n");
+    }
 
     @Override
     public void onError(Throwable throwable) {}
