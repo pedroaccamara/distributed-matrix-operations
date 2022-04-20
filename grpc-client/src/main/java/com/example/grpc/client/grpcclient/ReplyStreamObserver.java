06f29a3dbf8a929ae82da3d20ecbebdfa4dce753
pedroaccamara <pedroaccamara@gmail.com>
2022-04-20 03:59:00 +0100

    Return MatrixReply

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/ReplyStreamObserver.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/ReplyStreamObserver.java
@@ -15,6 +15,11 @@ public class ReplyStreamObserver implements StreamObserver<MatrixReply> {
     }
 
     @Override
+    public MatrixReply onNext(MatrixReply reply) {
+        return reply;
+    }
+
+    @Override
     public void onError(Throwable throwable) {}
 
     @Override
